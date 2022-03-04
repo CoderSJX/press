257413bb014961135aad6685f30c67485f5bc10b
sjx <550068683@qq.com>
2022-03-04 23:09:36 +0800

    电脑快捷键

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
                 '变量命名规范','说说MySQL的那些日志','LeetCode刷题记录','快速理解SSM三大流行框架','Maven安装及设置'
             ],
             '/stars/':['前端那些Amazing的网站','程序员学习网站精选','学术研究类工具推荐','程序员的工具箱',],
+            '/small-white/':['常用电脑快捷键（Windows系统）',],
 
             '/': [
                 '',
@@ -22,7 +23,7 @@ module.exports = {
             { text: '资源', link: '/resources/' },
             { text: '推荐', link: '/recommend/' },
             { text: '经验总结', link: '/summary/变量命名规范' },
-            { text: '电脑小白', link: '/small-white/' },
+            { text: '电脑小白', link: '/small-white/常用电脑快捷键（Windows系统）' },
             { text: '情感分享', link: '/emotion/' },
             { text: 'Github', link: 'https://github.com/CoderSJX' },
         ]
