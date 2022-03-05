d79effcfeca7b9f8b0124b36049589004cb6c44b
sjx <550068683@qq.com>
2022-03-05 12:01:16 +0800

    支持全文索引和首页更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    title:'idea小时',
-    dest:'dist',
+    title: 'idea小时',
+    dest: 'dist',
     themeConfig: {
 
         sidebar: {
-            '/summary/':[
-                '变量命名规范','说说MySQL的那些日志','LeetCode刷题记录','快速理解SSM三大流行框架','Maven安装及设置'
+            '/summary/': [
+                '变量命名规范', '说说MySQL的那些日志', 'LeetCode刷题记录', '快速理解SSM三大流行框架', 'Maven安装及设置'
             ],
-            '/stars/':['前端那些Amazing的网站','程序员学习网站精选','学术研究类工具推荐','程序员的工具箱',],
-            '/small-white/':['常用电脑快捷键（Windows系统）','电脑选购指南'],
+            '/stars/': ['前端那些Amazing的网站', '程序员学习网站精选', '学术研究类工具推荐', '程序员的工具箱',],
+            '/small-white/': ['常用电脑快捷键（Windows系统）', '电脑选购指南'],
 
             '/': [
                 '',
@@ -16,16 +16,20 @@ module.exports = {
 
         },
 
-        logo:'/favicon.ico',
+        logo: '/favicon.ico',
         nav: [
-            { text: '主页', link: '/' },
-            { text: '收藏夹', link: '/stars/前端那些Amazing的网站' },
-            { text: '资源', link: '/resources/' },
-            { text: '推荐', link: '/recommend/' },
-            { text: '经验总结', link: '/summary/变量命名规范' },
-            { text: '电脑小白', link: '/small-white/常用电脑快捷键（Windows系统）' },
-            { text: '情感分享', link: '/emotion/' },
-            { text: 'Github', link: 'https://github.com/CoderSJX' },
+            {text: '主页', link: '/'},
+            {text: '收藏夹', link: '/stars/前端那些Amazing的网站'},
+            {text: '资源', link: '/resources/'},
+            {text: '推荐', link: '/recommend/'},
+            {text: '经验总结', link: '/summary/变量命名规范'},
+            {text: '电脑小白', link: '/small-white/常用电脑快捷键（Windows系统）'},
+            {text: '情感分享', link: '/emotion/'},
+            {text: 'Github', link: 'https://github.com/CoderSJX'},
         ]
-    }
+    },
+
+    // ...
+    plugins: ['fulltext-search'],
+
 }
