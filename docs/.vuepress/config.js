255f902b61de693a8e0cebca423836ad37f3ff28
sjx <550068683@qq.com>
2022-03-06 18:59:03 +0800

    pwa 背景颜色

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,13 @@ module.exports = {
     dest: 'dist',
 
     head: [
-        ['link', {rel: 'icon', href: '/logo.ipg'}],
+        ['link', {rel: 'icon', href: '/logo.jpg'}],
         ['link', {rel: 'manifest', href: '/manifest.json'}],
-        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        ['meta', {name: 'theme-color', content: '#409EFF'}],
         ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
         ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
         ['link', {rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.jpg'}],
-        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
+        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#409EFF'}],
         ['meta', {name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.jpg'}],
         ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
     ],
