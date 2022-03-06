564741a72f7bc7357df101eb3914626f677ae3e6
sjx <550068683@qq.com>
2022-03-06 18:40:19 +0800

    pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
     title: 'idea小时',
     dest: 'dist',
+
+    head: [
+        ['link', {rel: 'icon', href: '/logo.ipg'}],
+        ['link', {rel: 'manifest', href: '/manifest.json'}],
+        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+        ['link', {rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.jpg'}],
+        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
+        ['meta', {name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.jpg'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
+    ],
     locales: {
         '/': {
             lang: 'zh-CN'
@@ -35,7 +47,13 @@ module.exports = {
     },
 
     // ...
-    plugins: ['fulltext-search', '@vuepress/back-to-top', '@vuepress/nprogress', '@vuepress/medium-zoom', [
+    plugins: ['fulltext-search', '@vuepress/back-to-top', '@vuepress/nprogress', '@vuepress/medium-zoom', '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+            message: "发现新内容可用",
+            buttonText: "刷新"
+        }
+    }, [
         '@vuepress/last-updated',
         {
             transformer: (timestamp, lang) => {
