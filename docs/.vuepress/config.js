c57c870f613b6a0ed656edc3b32c9a8d93fa6d4c
sjx <550068683@qq.com>
2022-03-06 20:48:57 +0800

    调试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,14 +58,14 @@ module.exports = {
         }
     }, [
         '@vuepress/last-updated',
-        // {
-        //     transformer: (timestamp, lang) => {
-        //         // 不要忘了安装 moment
-        //         const moment = require('moment')
-        //         moment.locale(lang)
-        //         return moment(timestamp).fromNow()
-        //     }
-        // }
+        {
+            transformer: (timestamp, lang) => {
+                // 不要忘了安装 moment
+                const moment = require('moment')
+                moment.locale(lang)
+                return moment(timestamp).fromNow()
+            }
+        }
     ], [
         '@vuepress/google-analytics',
         {
