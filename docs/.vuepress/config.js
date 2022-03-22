00e88383ec854bf13c4e836c38b25a941e8f9157
sjx <550068683@qq.com>
2022-03-22 18:52:29 +0800

    导航栏改造

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,12 +81,6 @@ module.exports = {
         logo: '/favicon.ico',
         nav: [
             {text: '首页', link: '/'},
-            {text: '网站收藏夹', link: '/stars/research-util/'},
-            {text: '资源分享', link: '/resources/library-for-coder/'},
-            {text: '推荐与避坑', link: '/recommend/house-rent/'},
-            {text: '编程之路', link: '/summary/spring-boot-annotation/'},
-            {text: '电脑小白', link: '/small-white/short-cut-for-windows/'},
-            {text: '情感分享', link: '/emotion/psychology'},
             {text: '时间轴', link: '/timeline/',},
             {text: 'Github', link: 'https://github.com/CoderSJX'},
         ],
